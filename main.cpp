040aa11d7609f67568d857e01e57d8afae76d299
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-04-04 19:20:02 +0200

    small c++ practice to get back into it

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
-int main()  {
-	std::cout << "Salam alaikoum" << std::endl; 
-	} 
+bool isPrime(int value) {
+	for(int i = 2 ; i < value / 2; i++){
+		if (value % i == 0) return false ;
+	}
+	return true ;
+}
+
+int main() {
+	int prime ; 
+	std::cout << "Enter a prime number\n" << std::endl; 
+	std::cin >> prime ;
+	if(isPrime(prime)) {
+		std::cout << "this number is prime" << std::endl; 
+	}
+	else {
+		std::cout << "this number is not prime\n";
+	}
+}
